2e3caf7b8a7dd1eb74bd50dea71ab6c86a3f30aa
Coby-Burckard <51294008+Coby-Burckard@users.noreply.github.com>
2020-05-25 10:25:48 -0500

    updating ports

--- stockAPI/stockAPI.js
+++ stockAPI/stockAPI.js
@@ -1,6 +1,7 @@
 const WebSocket = require('ws');
 const fs = require('fs');
 const http = require('http');
+const express = require('express');
 const Stock = require('./features/Stock');
 const UserList = require('./features/UserList');
 const APIparams = require('./constants/APIConstants');
@@ -8,10 +9,20 @@ const { manageStockServer } = require('./stockServer/stockServer');
 
 const startStockAPI = () => {
   //initializing API data storage
-  const stocks = APIparams.map(({ symbol, APIKey }) =>
-    Stock.fromSymbol(symbol, APIKey)
-  );
   const users = new UserList();
+  const production = false;
+  let stocks;
+
+  if (production) {
+    stocks = {
+      AAPL: process.env.STOCK_ONE,
+      AMZN: process.env.STOCK_TWO,
+    };
+  } else {
+    stocks = APIparams.map(({ symbol, APIKey }) =>
+      Stock.fromSymbol(symbol, APIKey)
+    );
+  }
 
   // itializing server for client connections
   const server = http.createServer();
